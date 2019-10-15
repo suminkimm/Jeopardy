4eb3dd611e12d57dd0716b50d07e8eb0134a60fa
Sumin Kim <SuminKim@nbp-235-34.nbp.ruw.rutgers.edu>
2019-10-15 19:06:46 -0400

    Fixing db connection

--- config_db.php
+++ config_db.php
@@ -1,15 +1,20 @@
 <?php
 
-$dsn = "pgsql:"
-    . "host=ec2-107-20-230-70.compute-1.amazonaws.com;"
-    . "dbname=da19331kqa55ff;"
-    . "user=frmkuzmbtknfgk;"
-    . "port=5432;"
-    . "sslmode=require;"
-    . "password=689d7618f122e2f0f20d0bf20489d5d8e77617dde052d9b1bc5af61facd6b4fd";
+//$dsn = "pgsql:"
+//    . "host=ec2-107-20-230-70.compute-1.amazonaws.com;"
+//    . "dbname=da19331kqa55ff;"
+//    . "user=frmkuzmbtknfgk;"
+//    . "port=5432;"
+//    . "sslmode=require;"
+//    . "password=689d7618f122e2f0f20d0bf20489d5d8e77617dde052d9b1bc5af61facd6b4fd";
+//
+//$conn = new PDO($dsn);
+//$conn = pg_connect("host=c2-107-20-230-70.compute-1.amazonaws.com dbname=da19331kqa55ff user=frmkuzmbtknfgk password=689d7618f122e2f0f20d0bf20489d5d8e77617dde052d9b1bc5af61facd6b4fd");
 
-$conn = new PDO($dsn);
-$conn = pg_connect("host=c2-107-20-230-70.compute-1.amazonaws.com dbname=da19331kqa55ff user=frmkuzmbtknfgk password=689d7618f122e2f0f20d0bf20489d5d8e77617dde052d9b1bc5af61facd6b4fd");
+$db_url = getenv("DATABASE_URL") ?: "postgres://frmkuzmbtknfgk:689d7618f122e2f0f20d0bf20489d5d8e77617dde052d9b1bc5af61facd6b4fd@ec2-107-20-230-70.compute-1.amazonaws.com:5432/da19331kqa55ff
+";
+$conn = pg_connect($db_url);
+
 if (!$conn) {
     echo "Database connection failed.";
     echo pg_last_error($conn);
