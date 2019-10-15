64c01437c9b6976dd248a8a58ee55d422be48bab
Sumin Kim <SuminKim@nbp-235-34.nbp.ruw.rutgers.edu>
2019-10-15 16:01:27 -0400

    Edited database config to PDO

--- config_db.php
+++ config_db.php
@@ -1,13 +1,25 @@
 <?php
 
-$db = parse_url(getenv("postgres://frmkuzmbtknfgk:689d7618f122e2f0f20d0bf20489d5d8e77617dde052d9b1bc5af61facd6b4fd@ec2-107-20-230-70.compute-1.amazonaws.com:5432/da19331kqa55ff
-"));
-$db["path"] = ltrim($db["path"], "/"); // db name
+//$db = parse_url(getenv("postgres://frmkuzmbtknfgk:689d7618f122e2f0f20d0bf20489d5d8e77617dde052d9b1bc5af61facd6b4fd@ec2-107-20-230-70.compute-1.amazonaws.com:5432/da19331kqa55ff
+//"));
+//$db["path"] = ltrim($db["path"], "/"); // db name
+//
+//echo "db: ".$db;
+//
+//$conn = pg_connect(getenv("postgres://frmkuzmbtknfgk:689d7618f122e2f0f20d0bf20489d5d8e77617dde052d9b1bc5af61facd6b4fd@ec2-107-20-230-70.compute-1.amazonaws.com:5432/da19331kqa55ff
+//"));
 
-echo "db: ".$db;
 
-$conn = pg_connect(getenv("postgres://frmkuzmbtknfgk:689d7618f122e2f0f20d0bf20489d5d8e77617dde052d9b1bc5af61facd6b4fd@ec2-107-20-230-70.compute-1.amazonaws.com:5432/da19331kqa55ff
-"));
+$dsn = "pgsql:"
+    . "host=ec2-107-20-230-70.compute-1.amazonaws.com;"
+    . "dbname=da19331kqa55ff;"
+    . "user=frmkuzmbtknfgk;"
+    . "port=5432;"
+    . "sslmode=require;"
+    . "password=689d7618f122e2f0f20d0bf20489d5d8e77617dde052d9b1bc5af61facd6b4fd";
+
+$conn = new PDO($dsn);
+
 //$conn = pg_connect("postgres://frmkuzmbtknfgk:689d7618f122e2f0f20d0bf20489d5d8e77617dde052d9b1bc5af61facd6b4fd@ec2-107-20-230-70.compute-1.amazonaws.com:5432/da19331kqa55ff
 //");
 
