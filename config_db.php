2e06cd4769e66ffdff8afa2653c1ee0e9205ac28
Sumin Kim <SuminKim@nbp-235-34.nbp.ruw.rutgers.edu>
2019-10-15 15:54:10 -0400

    Edited database config again

--- config_db.php
+++ config_db.php
@@ -1,10 +1,20 @@
 <?php
 
-$db = parse_url(getenv("DATABASE_URL"));
+$db = parse_url(getenv("postgres://frmkuzmbtknfgk:689d7618f122e2f0f20d0bf20489d5d8e77617dde052d9b1bc5af61facd6b4fd@ec2-107-20-230-70.compute-1.amazonaws.com:5432/da19331kqa55ff
+"));
 $db["path"] = ltrim($db["path"], "/"); // db name
 
-$conn = pg_connect("postgres://frmkuzmbtknfgk:689d7618f122e2f0f20d0bf20489d5d8e77617dde052d9b1bc5af61facd6b4fd@ec2-107-20-230-70.compute-1.amazonaws.com:5432/da19331kqa55ff
-");
+echo "db: ".$db;
+
+$conn = pg_connect(getenv("postgres://frmkuzmbtknfgk:689d7618f122e2f0f20d0bf20489d5d8e77617dde052d9b1bc5af61facd6b4fd@ec2-107-20-230-70.compute-1.amazonaws.com:5432/da19331kqa55ff
+"));
+//$conn = pg_connect("postgres://frmkuzmbtknfgk:689d7618f122e2f0f20d0bf20489d5d8e77617dde052d9b1bc5af61facd6b4fd@ec2-107-20-230-70.compute-1.amazonaws.com:5432/da19331kqa55ff
+//");
+
+//$result = pg_query($conn, "SELECT datname FROM pg_database");
+//while ($row = pg_fetch_row($result)) {
+//    echo "<p>" . htmlspecialchars($row[0]) . "</p>\n";
+//}
 
 if (!$conn) {
     echo "Database connection failed.";
@@ -13,7 +23,3 @@ if (!$conn) {
 else {
     echo "Database connection success.";
 }
-//$result = pg_query($conn, "SELECT datname FROM pg_database");
-//while ($row = pg_fetch_row($result)) {
-//    echo "<p>" . htmlspecialchars($row[0]) . "</p>\n";
-//}
