0090c77d04c0aab8b74ae8c1a5ddce9a6270c2f6
Sumin Kim <SuminKim@nbp-211-196.nbp.ruw.rutgers.edu>
2019-10-16 09:50:26 -0400

    Fix adding to questions table for favorites function

--- changeFavorited.php
+++ changeFavorited.php
@@ -4,10 +4,10 @@ session_start();
 require('config_db.php');
 
 $user_id = $_SESSION['user_id'];
-$question = $_GET['q'];
-$ans = $_GET['a'];
-$airdate = $_GET['air'];
-$category = $_GET['cat'];
+$question = pg_escape_literal($_GET['q']);
+$ans = pg_escape_literal($_GET['a']);
+$airdate = pg_escape_literal($_GET['air']);
+$category = pg_escape_literal($_GET['cat']);
 $difficulty = $_GET['d'];
 $q_id = $_GET['qid'];
 
@@ -21,12 +21,12 @@ if (pg_num_rows($result) != 0) { // already exists, delete
 
 else {
     // add into questions table if it doesn't already exist
+
     $sql = " INSERT INTO public.questions (question, answer, airdate, category, difficulty, q_id) 
-        VALUES ($question, $ans, $airdate, $category, $difficulty, $q_id)
+        VALUES ('$question', '$ans', '$airdate', '$category', '$difficulty', '$q_id')
         WHERE NOT EXISTS (
         SELECT 1 FROM public.questions 
-        WHERE q_id = $q_id)";
-    $sql = pg_escape_literal($conn, $sql);
+        WHERE q_id = '$q_id')";
     $insert_question = pg_query($conn, $sql);
 
     echo $question;
